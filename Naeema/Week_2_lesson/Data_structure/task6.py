9df0528088a91df3d6cbf640fbf51e41b2378d09
Naeema <naeemaadenike@gmail.com>
2025-08-15 13:14:25 +0100

    task 6

---
+++ Naeema/Week_2_lesson/Data_structure/task6.py
@@ -0,0 +1,5 @@
+# fruits = input("Enter 5 favourite fruits").split()
+# empty_set = set(fruits)
+# print(empty_set)
+
+
---
+++ Naeema/Week_2_lesson/Data_structure/task6/task_1.py
@@ -0,0 +1,7 @@
+fruits1= input("Enter your favourite fruit: ")
+fruits2= input("Enter your favourite fruit: ")
+fruits3= input("Enter your favourite fruit: ")
+fruits4= input("Enter your favourite fruit: ")
+fruits5= input("Enter your favourite fruit: ")
+empty_set = {fruits1, fruits2, fruits3, fruits4, fruits5}
+print(empty_set)
