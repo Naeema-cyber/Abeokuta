3fd774b17726f632e6b401aab8d70c645418968d
Naeema <naeemaadenike@gmail.com>
2025-08-21 15:18:11 +0100

    new one

--- Naeema/Week_2_lesson/Data_structure/task6.py
+++
@@ -1,5 +0,0 @@
-# fruits = input("Enter 5 favourite fruits").split()
-# empty_set = set(fruits)
-# print(empty_set)
-
-
---
+++ Naeema/Week_2_lesson/Data_structure/task7/task_1.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+gender = input("State your gender: ")
+empty_list = []
+courses = list(input("Your courses: "))
+print(f"\tName: {name}\n\tAge: {age}\n\tGender: {gender}\n\tCourses: {courses} ")
+
+
