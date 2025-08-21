e22043b0da057a25e1f83176e7765b26920ccd7c
Naeema <naeemaadenike@gmail.com>
2025-08-21 12:23:02 +0100

    new file

--- Naeema/week_3_lesson/task_4.py
+++ Naeema/week_3_lesson/task_4.py
@@ -1,5 +1,5 @@
 student = {}
-name = input("Enter your name: ")
+name = str(input("Enter your name: "))
 age = int(input("Enter your age: "))
 scores = [70, 85, 90]
 
@@ -7,4 +7,9 @@ student.update({"Name": name})
 student.update({"Age": age})
 student.update({"Scores": scores})
 
-average_score = (student[scores][0] + student[scores][1] + student[scores][2])/ 4
+avg_score = (scores[0] + scores[1] + scores[2])/ 4
+student["passed"] = avg_score>=50
+
+teenager = (student["Age"] >= 13) and (student['Age'] <= 19)
+
+print(f"student:\n Name: {student['Name']} \n Age: {student['Age']}\n Scores:{student["Scores"]}\n Passed:{student['passed']}\nTeenager:{teenager}")
