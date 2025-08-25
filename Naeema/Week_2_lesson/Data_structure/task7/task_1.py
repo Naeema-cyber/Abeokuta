2d5ce83914f4b36294b96286c2e2c3f1a920fd36
Adebiyi Naheemot <naeemaadenike@gmail.com>
2025-08-25 09:50:45 +0100

    new

--- Naeema/Week_2_lesson/Data_structure/task7/task_1.py
+++ Naeema/Week_2_lesson/Data_structure/task7/task_1.py
@@ -1,8 +1,13 @@
 name = input("Enter your name: ")
-age = input("Enter your age: ")
+age = int(input("Enter your age: "))
 gender = input("State your gender: ")
 empty_list = []
-courses = list(input("Your courses: "))
-print(f"\tName: {name}\n\tAge: {age}\n\tGender: {gender}\n\tCourses: {courses} ")
+course1 = input("Your course: ")
+course2 = input("Enter the second course: ")
+course3 = input("Enter the third course: ")
+empty_list.append("{course1}")
+empty_list.append("{course2}")
+empty_list.append("{course3}")
 
 
+print(f"\tName: {name}\n\tAge: {age}\n\tGender: {gender}\n\tCourses: {empty_list} ")
