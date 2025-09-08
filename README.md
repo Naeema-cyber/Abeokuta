06160f7d7ce586c19b0e25e252780b2867692acc
Naeema <naeemaadenike@gmail.com>
2025-09-08 10:59:49 +0100

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# abeokuta
+# ABEOKUTA
---
+++ my_new_virtual_environment/practicing.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import csv
+import json
+
+
+file_path = "practical.json"
+
+
+
+with open (file_path, "a", encoding="utf-8") as f:
+    f.write("I am a product of Allah's rahma.\n")
+    f.write("Allah is ever faithful to the believers.\n")
